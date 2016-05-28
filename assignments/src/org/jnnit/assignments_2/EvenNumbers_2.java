30f6cba94fefe4e229215e28d38b1a84ebe0e264
ravikiran88 <ravikiranyadava@gmail.com>
2016-05-27 19:31:10 -0500

    testing git skills

--- assignments/src/org/jnnit/assignments_2/EvenNumbers_2.java
+++ assignments/src/org/jnnit/assignments_2/EvenNumbers_2.java
@@ -4,6 +4,7 @@ public class EvenNumbers_2 {
 
 	public static void main(String[] args) {
 		System.out.println("Even Numbers between 2 and 100");
+		System.out.println("Changed to test git functionality");
 		for(int i=1;i<=100;i++){
 			if(i%2==0){
 				System.out.println(i);
